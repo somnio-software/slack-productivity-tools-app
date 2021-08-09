736a46098aea29e48f937249778e20934efdc3d3
Mauri Pastorini <mpastorini@somniosoftware.com>
2021-08-09 09:35:16 -0300

    Extracted keys

--- functions/index.js
+++ functions/index.js
@@ -5,38 +5,45 @@ const slackToken = require("./keys.json")["slack-token"]; //xoxp slack app token
 const { WebClient } = require("@slack/web-api");
 
 exports.setFocus = functions.https.onRequest(async (request, response) => {
-	const web = new WebClient(slackToken);
-	const text = request.body.text || "for 1 hour";
-	const timeArr = text.split("for ");
-	const time = timeArr.length > 1 ? timeArr[1] : "1 hour";
-	functions.logger.info("Time: ", text);
-	const res = await web.dnd.setSnooze({ num_minutes: time });
-	functions.logger.info("Snoose set: ", res);
+	try {
+		functions.logger.info("Token: ", slackToken);
+		const web = new WebClient(slackToken);
+		const fullTime = request.body.text || "for 1 hour";
+		functions.logger.info("Full Time: ", fullTime);
+		const timeArr = fullTime.split("for ");
+		const time = timeArr.length > 1 ? timeArr[1] : "1 hour";
+		functions.logger.info("Time formated: ", time);
+		const res = await web.dnd.setSnooze({ num_minutes: time });
+		functions.logger.info("Snoose set: ", res);
 
-	// TODO: To be implemented: Don't create reminders, just a message from the app
-	// const user = await web.users.identity({});
-	// functions.logger.info("Hello USer!", user);
+		// TODO: To be implemented: Don't create reminders, just a message from the app
+		// const user = await web.users.identity({});
+		// functions.logger.info("Hello USer!", user);
 
-	// const postMessageRes = await web.chat.postMessage({
-	//     text: "Focus off",
-	// 	channel: user.id,
-	// });
+		// const postMessageRes = await web.chat.postMessage({
+		//     text: "Focus off",
+		// 	channel: user.id,
+		// });
 
-	const reminderRes = await web.reminders.add({
-		text: "Focus off",
-		time: time,
-	});
-	functions.logger.info("Reminder created: ", reminderRes);
-	const profile = {
-		status_text: `Focusing ${text}`,
-		status_emoji: ":nerd_face:",
-		status_expiration: res.snooze_endtime,
-	};
-	functions.logger.info("Profile updated: ", profile);
-	await web.users.profile.set({ profile });
-	response.send(
-		`
-        Result: Do not disturb and reminder ${time}. 
-        `
-	);
+		const reminderRes = await web.reminders.add({
+			text: "Focus off",
+			time: time,
+		});
+		functions.logger.info("Reminder created: ", reminderRes);
+		const profile = {
+			status_text: `Focusing ${fullTime}`,
+			status_emoji: ":nerd_face:",
+			status_expiration: res.snooze_endtime,
+		};
+		functions.logger.info("Profile updated: ", profile);
+		await web.users.profile.set({ profile });
+		response.send(
+			`
+				Result: Do not disturb and reminder ${time}. 
+				`
+		);
+	} catch (err) {
+		functions.logger.info("Error: ", err);
+		throw err;
+	}
 });
